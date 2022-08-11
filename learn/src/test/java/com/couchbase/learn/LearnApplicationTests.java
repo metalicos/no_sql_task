50cf6b47a124bc2aab0e11881add815d6db352bd
Ostap Komplikevych <ostap.ja@gmail.com>
2022-08-11 14:31:05 +0300

    init project

---
+++ learn/src/test/java/com/couchbase/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couchbase.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
